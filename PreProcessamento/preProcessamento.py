564f921b5e02dc0cb428888d1266288d6d753a74
Fábio Rezende <rezendesza@gmail.com>
2022-10-18 18:19:00 -0300

    Adicionando Pré-processamento, modelos estatisticos e word embeddings

--- PreProcessamento/preProcessamento.py
+++ PreProcessamento/preProcessamento.py
@@ -1,22 +1,51 @@
 import pandas as pd 
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer 
+import spacy 
+import re
 
 class PreProcessamento:
 
-    def __init__(self,  data):
-        self.data = data
+    def __init__(self, fonte):
+        self.fonte = fonte
+        self.stemmer = PorterStemmer()
+        self.lemmatizer = WordNetLemmatizer()
+        self.nlp = spacy.load('en_core_web_sm')
 
-    def pipeline(self):
-        return pd.Series()
+    def nlp_pipeline(self, texto):
 
-    def normalizacao(self):
-        pass 
+        doc = self.nlp(texto)
 
-    def tokenizacao(self):
-        pass
+        tokens, pos_tags, lemmas = [], [], []
+
+        for token in doc:
+            tokens.append(token.text)
+            pos_tags.append(token.pos_)
+            lemmas.append(token.lemma_)
+
+        return tokens, pos_tags, lemmas
+
+    def normalizacao(self, texto):
+        texto_normalizado = texto.lower()
+        return texto_normalizado
+
+    def tokenizacao(self, texto):
+        tokens = word_tokenize(texto)
+        return tokens
     
-    def stemmizacao(self):
-        pass
+    def stemmizacao(self, tokens):
+        stem = self.stemmer.stem
+        stems = [stem(t) for t in tokens]
+        return stems
 
-    def lemmatizacao(self):
-        pass
+    def lemmatizacao(self, tokens):
+        lemma = self.lemmatizer.lemmatize
+        lemmas = [lemma(t) for t in tokens]
+        return lemmas
     
+    def limpeza_regex(self, texto, padrao, valor=" "):
+
+        texto = re.sub(padrao, " {valor} ".format(valor=valor), texto)
+
+        return texto
