70cd4dbe89ca64663a0d5c48c7b90e7289068436
Jordan Kobellarz <jordan.kobellarz@gmail.com>
2022-11-02 01:35:29 -0300

    Added topic modeling section

---
+++ ExtracaoConhecimento/TopicModeling.py
@@ -0,0 +1,18 @@
+
+"""
+
+Authors:
+
+    Fabio Rezende (fabiorezende@usp.br)
+    Frances Santos (frances.santos@ic.unicamp.br)
+    Jordan Kobellarz (jordan@alunos.utfpr.edu.br)
+
+Updated in Sep 28th , 2022.
+
+"""
+
+
+class TopicModeling:
+
+    def lda(self):
+        pass
